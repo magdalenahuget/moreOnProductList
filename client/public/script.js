df217ab7a7bc343e496c96fa9c3a8116fac121c8
magdalenahuget <magdalena.huget@gmail.com>
2023-01-06 20:36:43 +0100

    Add task 2.

--- client/public/script.js
+++ client/public/script.js
@@ -1,10 +1,44 @@
 import { products } from '/data.js';
 
-// TASK 1 - Rearrange by price
+// // TASK 1 - Rearrange by price
+
+// const divElement = function (content) {
+
+//     return `<div>${content.name}</div>`;
+// }
+
+
+// const loadEvent = function () {
+
+//     // Write your JavaScript code after this line
+//     products.sort((a, b) => (a.price > b.price) ? 1 : -1);
+
+//     let rootElement = document.getElementById("root");
+
+//     products.forEach((product) => {
+//         rootElement.insertAdjacentHTML("beforeend", divElement(product));
+//     })
+
+//     // Write your JavaScript code before this line
+
+// }
+
+// window.addEventListener("load", loadEvent);
+
+
+
+
+
+// // TASK 2 - Remove cheap ones
 
 const divElement = function (content) {
 
-    return `<div>${content.name}</div>`;
+    let result = "";
+        if (content.price > 1000) {
+            result += `${content.name}<br />`;
+        }
+
+    return `<div>${result}</div>`;
 }
 
 
